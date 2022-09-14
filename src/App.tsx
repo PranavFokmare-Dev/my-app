3b4788527cfdf239dd92a2082741eeff26489e6a
Pranav <pranav.fokmare.developer@gmail.com>
2022-09-14 09:06:07 +0530

    adding bootstrap to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,28 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./features/Headder/Header";
 import Home from "./features/Home/Home";
 import AboutUs from "./features/About/AboutUs";
+import Courses from "./features/courses/Courses";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/About" element={<AboutUs />}></Route>
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/About" element={<AboutUs />}></Route>
+          <Route path="/Courses" element={<Courses />}></Route>
+          <Route
+            path="*"
+            element={
+              <div>
+                <h1>404 Not found</h1>
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/features/Headder/Header.tsx
+++ src/features/Headder/Header.tsx
@@ -1,31 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, NavDropdown } from "react-bootstrap";
+
 const activeStyles = {
   color: "red",
   borderBottom: "1px solid black",
 };
 export default function Header() {
   return (
-    <nav>
-      <NavLink
-        to="/"
-        style={({ isActive }) =>
-          isActive ? activeStyles : { color: "black", borderBottom: "0px" }
-        }
-        end
-      >
-        Home
-      </NavLink>
-      {"|"}
-      <NavLink
-        to="/About"
-        style={({ isActive }) =>
-          isActive ? activeStyles : { color: "black", borderBottom: "0px" }
-        }
-        end
-      >
-        AboutUs
-      </NavLink>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/About">
+              About us
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/Courses">
+              Courses
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
